152c38c7c105d79e9ee80596de8fe01e24716491
Robert M Hendricks <rmasonh@me.com>
2019-09-22 13:29:27 -0400

    add todo and button to test

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  title: string;
+}
+
 @Component({
   selector: 'myorg2-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todos';
+ todos: Todo[] = [{ title: 'Todo1'}, {title: 'Todo2'}];
+
+ addTodo() {
+   this.todos.push({
+     title: 'New todo ${Math.random() * 1000)}'
+   });
+ }
 }
